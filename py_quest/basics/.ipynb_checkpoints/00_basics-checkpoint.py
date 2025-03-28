49d9e67e3f5db19e828522014245d5280d3233c1
niya <niya123op@gmail.com>
2025-03-28 22:58:12 +0530

    removed jupyter

---
+++ py_quest/basics/.ipynb_checkpoints/00_basics-checkpoint.py
@@ -0,0 +1,14 @@
+# Python runs from first line onwards
+# 'print()' : prints things you put inside it.
+print("hello world")
+print("Hello world1")
+# '\n' is used to cut to the next line
+print("hello \nworlds apart")
+
+# PRINT MULIPLE VALUES
+
+#concatenate: using '+' sign. It only works for strings.
+print("Hello, " + "How are you " + "fine thankyu")
+
+# the comma method: works for pretty much any value
+print("hi, i'm ", 7, " and am i smart? ", True)
