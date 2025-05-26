17f11df5d3ee7ed91cd9921a1e7c8d68f672f29d
niya <niya123op@gmail.com>
2025-05-27 00:12:50 +0530

    Added a section about using a list as the default argument's value and how it's stupid

--- py_quest/functions/default_arg.py
+++ py_quest/functions/default_arg.py
@@ -15,3 +15,19 @@ def count(end,start=0):
         start += 1
         
 count(10,1)
+
+## NEVER USE "=[]" AS A DEFAULT ARGUMENT:
+#def worng_way(name, ingredients=[]):
+
+# solution: 
+def dish(name, ingredients=None):
+    if ingredients is None:
+        return "The input isn't valid"
+    else:
+        return f"The {name} is made with {', '.join(ingredients)}"
+
+
+dish_name = input("what dish do you like : ")
+dish_items = input("what are the ingredieants? (use space to seperate items)").split(" ")
+
+print(dish(dish_name, dish_items))
